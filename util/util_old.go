f891d14501d8c52e272ebcc02d441dae775fa5e0
jarryliu <jarrysix@gmail.com>
2018-11-16 14:45:57 +0800

    generator

--- util/util_old.go
+++ util/util_old.go
@@ -14,25 +14,6 @@ import (
 	"log"
 )
 
-/*
-//编码
-func EncodingTransform(src []byte, enc string) ([]byte, error) {
-	var ec encoding.Encoding
-	switch enc {
-	default:
-		return src, nil
-	case "GBK":
-		ec = simplifiedchinese.GBK
-	case "GB2312":
-		ec = simplifiedchinese.HZGB2312
-	case "BIG5":
-		ec = traditionalchinese.Big5
-	}
-	dst := make([]byte, len(src)*2)
-	n, _, err := ec.NewEncoder().Transform(dst, src, true)
-	return dst[:n], err
-}
-*/
 
 // 强制序列化为可用于HTML的JSON
 func MustHtmlJson(v interface{}) template.JS {
