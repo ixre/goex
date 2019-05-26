f858e2c901cfd557a6a2b7134396ffbbf05932bd
jarrysix <jarrysix@gmail.com>
2019-05-26 09:47:15 +0800

    README.md

--- generator/bin/README.md
+++ generator/bin/README.md
@@ -1,6 +1,28 @@
+# 代码生成器
 
+使用Go编写的代码生成器,可根据模板定制生成代码.
 
+特点:
+- 支持多mysql和postgresql数据库
+- 支持Go,JAVA,Kotlin,Html,C#语言
+- 支持代码模板, 支持模板函数
 
+资源:
+- [下载地址](https://github.com/ixre/goex/releases/)
+- [Go模板语法-中](http://www.g-var.com/posts/translation/hugo/hugo-21-go-template-primer/)
+- [Go模板语法-English](https://golang.org/pkg/text/template/)
+
+## 快速开始
+
+1. 配置数据源
+```
+下载安装包,解压修改gen.conf文件进行数据源配置.
+```
+2. 定制修改模板
+```
+根据实际需求对模板进行修改, 或创建自己的模板. 模板语法请参考: Go Template
+```
+3. 运行命令生成代码
 ```bash
-go run gof-gen.go -conf=gen_pg.conf
+gof-gen -conf gen.conf
 ```
--- generator/bin/build.sh
+++ generator/bin/build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-CGO_ENABLED=0 GOOS=linux ARCH=amd64 go build -o gof-gen_linux gof-gen.go
-CGO_ENABLED=0 GOOS=darwin ARCH=amd64 go build -o gof-gen_osx gof-gen.go
+CGO_ENABLED=0 GOOS=linux ARCH=amd64 go build -o gof-gen gof-gen.go
+CGO_ENABLED=0 GOOS=darwin ARCH=amd64 go build -o mac-gof-gen gof-gen.go
 CGO_ENABLED=0 GOOS=windows ARCH=amd64 go build -o gof-gen.exe gof-gen.go
 
-tar cvzf generator_build.tar.gz gof-gen.sh gof-gen_* gof-gen.exe gen.conf gen_pg.conf templates
-rm -rf gof-gen_* gof-gen.exe
+tar cvzf generator-build-bin.tar.gz gof-gen.sh mac-gof-gen gof-gen\
+ gof-gen.exe gen.conf templates README.md
+
+rm -rf gof-gen mac-gof-gen gof-gen.exe
